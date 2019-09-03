c72276101f6ddaf397903015048e3502ee698a77
Ondrej Novak <ondra-novak@email.cz>
2019-09-03 14:24:39 +0200

    Hide donation button option UI - reenable feature
    Conflicts:
    	www/res/code.js

--- www/res/code.js
+++ www/res/code.js
@@ -200,6 +200,8 @@ function app_start(){
 		price:"np",
 		pl:"pln"
 	};
+	var show_donate = !localStorage["donation_hidden"]
+	
 	
 
 	var chartlist = {};
@@ -600,7 +602,7 @@ function app_start(){
 		for (var z in chart) {
 			sumchart = sumchart.concat(chart[z].slice(-20));			
 		}
-		if (donate_time) {
+		if (donate_time &&  show_donate) {
 			sumchart.push({
 				time: donate_time,
 				donation: true
@@ -1222,6 +1224,11 @@ function donate() {
 			var content = x.innerText;
 			x.innerHTML="<a href=\""+x.dataset.link+":"+content+"\">"+content+"</a>";
 		});
+		var hd = document.getElementById("hide_donation");
+		hd.addEventListener("change", function() {
+			localStorage["donation_hidden"] = hd.checked?hd.value:"";
+		});
+		hd.checked = localStorage["donation_hidden"] === hd.value;
 	}
 }
 function close_donate() {
