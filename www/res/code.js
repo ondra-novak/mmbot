c4f4203e5ea6e060e8d5f4e26ee9206b2779cadd
Ondrej Novak <ondra-novak@email.cz>
2019-09-19 23:26:12 +0200

    Show P/L instead cash flow

--- www/res/code.js
+++ www/res/code.js
@@ -558,7 +558,7 @@ function app_start(){
 					r.achg < 0?"↘":"↗",
 					Math.abs(r.achg),
 					r.price,
-					r.volume,
+					r.plDiff,
 					r.normch,
 				]
 				tr.classList.toggle("sell", r.achg<0);
