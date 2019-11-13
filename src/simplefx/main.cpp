942172868e213749e29cd8c95133455cd5bf2333
Ondrej Novak <ondra-novak@email.cz>
2019-11-13 21:54:16 +0100

    zakladni maketa simplefx, nefunkcni

---
+++ src/simplefx/main.cpp
@@ -0,0 +1,175 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 13. 11. 2019
+ *      Author: ondra
+ */
+#include <vector>
+
+#include "../brokers/api.h"
+#include <imtjson/value.h>
+#include <imtjson/object.h>
+
+using json::Object;
+using json::Value;
+
+
+
+static Value keyFormat = {Object
+							("name","key")
+							("type","string")
+							("label","Key"),
+						 Object
+							("name","secret")
+							("type","string")
+							("label","Secret")};
+
+class Interface: public AbstractBrokerAPI {
+public:
+
+	Interface(const std::string &path):AbstractBrokerAPI(path, keyFormat) {}
+
+	virtual double getBalance(const std::string_view & symb) override;
+	virtual TradesSync syncTrades(json::Value lastId, const std::string_view & pair) override;
+	virtual Orders getOpenOrders(const std::string_view & par)override;
+	virtual Ticker getTicker(const std::string_view & piar)override;
+	virtual json::Value placeOrder(const std::string_view & pair,
+			double size,
+			double price,
+			json::Value clientId,
+			json::Value replaceId,
+			double replaceSize)override;
+	virtual bool reset()override;
+	virtual MarketInfo getMarketInfo(const std::string_view & pair)override;
+	virtual double getFees(const std::string_view &pair)override;
+	virtual std::vector<std::string> getAllPairs()override;
+	virtual void enable_debug(bool enable) override;
+	virtual BrokerInfo getBrokerInfo() override;
+	virtual void onLoadApiKey(json::Value keyData) override;
+	virtual void onInit() override;
+
+};
+
+
+
+int main(int argc, char **argv) {
+	using namespace json;
+
+	if (argc < 2) {
+		std::cerr << "Required storage path" << std::endl;
+		return 1;
+	}
+
+	try {
+
+		Interface ifc(argv[1]);
+		ifc.dispatch();
+
+
+	} catch (std::exception &e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		return 2;
+	}
+}
+
+inline double Interface::getBalance(const std::string_view &symb) {
+	return 0;
+}
+
+inline Interface::TradesSync Interface::syncTrades(json::Value lastId,
+		const std::string_view &pair) {
+	return TradesSync();
+}
+
+inline Interface::Orders Interface::getOpenOrders(const std::string_view &par) {
+	return {};
+}
+
+inline Interface::Ticker Interface::getTicker(const std::string_view &piar) {
+	return {};
+}
+
+inline json::Value Interface::placeOrder(const std::string_view &pair,
+		double size, double price, json::Value clientId, json::Value replaceId,
+		double replaceSize) {
+	return nullptr;
+}
+
+inline bool Interface::reset() {
+	return true;
+}
+
+inline Interface::MarketInfo Interface::getMarketInfo(const std::string_view &pair) {
+	return {};
+}
+
+inline double Interface::getFees(const std::string_view &pair) {
+	return 0;
+}
+
+inline std::vector<std::string> Interface::getAllPairs() {
+	return {};
+}
+
+inline void Interface::enable_debug(bool enable) {
+
+}
+
+inline Interface::BrokerInfo Interface::getBrokerInfo() {
+	return {
+		false,
+		"simplefx",
+		"$impleFX",
+		"https://app.simplefx.com/",
+		"1.0",
+		"Copyright (c) 2019 Ondřej Novák\n\n"
+
+"Permission is hereby granted, free of charge, to any person "
+"obtaining a copy of this software and associated documentation "
+"files (the \"Software\"), to deal in the Software without "
+"restriction, including without limitation the rights to use, "
+"copy, modify, merge, publish, distribute, sublicense, and/or sell "
+"copies of the Software, and to permit persons to whom the "
+"Software is furnished to do so, subject to the following "
+"conditions: "
+"\n\n"
+"The above copyright notice and this permission notice shall be "
+"included in all copies or substantial portions of the Software. "
+"\n\n"
+"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, "
+"EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES "
+"OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND "
+"NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT "
+"HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, "
+"WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING "
+"FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR "
+"OTHER DEALINGS IN THE SOFTWARE.",
+"iVBORw0KGgoAAAANSUhEUgAAAIAAAACABAMAAAAxEHz4AAAAG1BMVEUxAAAXHy1XTFayQUbpTU6B"
+"hY2mq67GzM76/fq81alBAAAAAXRSTlMAQObYZgAAA8RJREFUaN7tmT9vozAUwA0kaUboETUjqu50"
+"GXNSPgCE+wDJgNSxXXoZ06VkTCKl+GOfnwEbGxsbMnThDYCd5x9+f+xnFIRGGWUUUdzoTsDyct94"
+"Z3n14RYEcCuvZYNcg4DqNO5BqdEw4IBxsUdoludkJtMi/4JJ5fkZoYecyOcbIRV5TnUL6Mk/BMAL"
+"JlIQbYwv9IpJ54o25pjKHjllL3LLDgEwxS0AmSGZ1ZUBbnSgDrCkXZcaAE0SFHI9MwCOugDkXfvH"
+"07EJOFLFLwooTtAhAY4SwEeLqAbM6XRmYJUPjUsA7mgAbnVA6hieqM2oBoBLr9QTmALOABMAchYc"
+"AMoBBzr5eQMwNwDAxuKDAUiDDKWhjUrAkwgo3t/fd+0oHDngRoZinGO8A8D1FxYBIAIAlX0VgGTM"
+"hYQJ498ArcP42glYlKHmgAM+YvzUBESdAPR4oDZQANE4r8CKB0iXCnBF3QAau3MJIIl9Bq98TaFV"
+"AooIdUXBIVkwYwDydIQkuLpVBG9ZFqBOwPzSBJAhR1heFwdmTsNYT10HeMFvwZz5gDztHLqSQFUA"
+"VOGEHeFVimJO/UIBK4gHLimFEiCvxocqUj4D+HR9wRqxAtBUpnocsIKVeLAFzHC5dEvACwCWsEHB"
+"kxUALf7hTygM0yzboyzL4InssQvyRLqqnMmo7NhdzITTvYXl+wHP4/FglG8XZ/O3JTHinQlT5L9b"
+"ADz2nNaKiq4OgOJ1k8bPZgAKWWNdKXK9yAbgye9zNRboAEjW99pu7QZwG3zJBcgO4ElO0FmgBfD+"
+"RMyCtSWA25B2WqAHuELYGG5rDUDNSXOttT0gbDjBlWJiBXAbZofqNBYA7Z82HPBTa0EXYMJfq0uC"
+"bgCLfeJq0tgAYDakod6CTgAbVxuT+v0Ajjo89gDufOVWYgHwxPEp6guQbEh6Axrbis4CDkiemWhs"
+"SG0LS6pKZ50FJkBossAEcE0WmAANG+JhgFC/lfQFoGEAkw97ODFBd4ZxUBSExbAeAPBMu4EJIG4I"
+"fm+AtJzXnYCtYjVKG8oW3bul+WjgplqD/qBh2zqrCykaVlh4ZfKHlbbY6UgFm+LKvakoTXbl3dMv"
+"ST3AVZ1Qkh6AUHlG6gEQxkzMh6xYVxe3Qiu2BkyEIY42nXUAbrUvLovYEuDpjvtbS0AoBU571NQB"
+"9J88sRXAa9WTsB9g0jLZM310xeqzTdw2KrYAeAqXbQwfnrEyjRvKPwyfvrEyBrGqTCZGQKgsBRob"
+"VICNcrZqbPk3iPS3R8BEpSn/RTLKKKN8t/wHuIduWNu27QwAAAAASUVORK5CYII="
+	};
+}
+
+inline void Interface::onLoadApiKey(json::Value keyData) {
+
+}
+
+inline void Interface::onInit() {
+}
+
