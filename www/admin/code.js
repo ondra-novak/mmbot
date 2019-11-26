b6b167fc71711de1970e85450a502ce738e6354b
Ondrej Novak <ondra-novak@email.cz>
2019-11-26 01:16:03 +0100

    improve ui - advance mode is global (show all)

--- www/admin/code.js
+++ www/admin/code.js
@@ -46,6 +46,7 @@ function App() {
 	this.traders={};
 	this.config={};
 	this.curForm = null;
+	this.advanced = false;
 }
 
 TemplateJS.View.prototype.showWithAnim = function(item, state) {	
@@ -493,7 +494,7 @@ App.prototype.fillForm = function (src, trg) {
 	}.bind(this),1);
 	}
 	data.broker_img = this.brokerImgURL(src.broker);
-	data.advanced = src.advanced;
+	data.advanced = this.advanced;
 
 	
 	data.strategy = (src.strategy && src.strategy.type) || "";
@@ -617,7 +618,7 @@ App.prototype.saveForm = function(form, src) {
 	trader.title = data.title;
 	trader.enabled = data.enabled;
 	trader.dry_run = data.dry_run;
-	trader.advanced = data.advanced;
+	this.advanced = data.advanced;
 	trader.accept_loss = data.accept_loss;
 	trader.spread_calc_stdev_hours =data.spread_calc_stdev_hours ;
 	trader.spread_calc_sma_hours  = data.spread_calc_sma_hours;
@@ -646,62 +647,6 @@ App.prototype.openTraderForm = function(trader) {
 	return Promise.resolve(form);	
 }
 
-TemplateJS.View.regCustomElement("X-SLIDER", new TemplateJS.CustomElement(
-		function(elem,val) {
-			var range = elem.querySelector("input[type=range]");
-			var number = elem.querySelector("input[type=number]");
-			var mult = parseFloat(elem.dataset.mult);
-			var fixed = parseInt(elem.dataset.fixed)
-			var toFixed = function(v) {
-				if (!isNaN(fixed)) return parseFloat(v).toFixed(fixed);
-				else return v;
-			}
-			if (!range) {
-				range = document.createElement("input");
-				range.setAttribute("type","range");
-				number = document.createElement("input");
-				number.setAttribute("type","number");
-				number.setAttribute("step",mult);
-				var env1 = document.createElement("div");
-				var env2 = document.createElement("div");
-				var min = parseFloat(elem.dataset.min);
-				var max = parseFloat(elem.dataset.max);
-				var rmin = Math.floor(min/mult);
-				var rmax = Math.floor(max/mult);
-				range.setAttribute("min",rmin);
-				range.setAttribute("max",rmax);
-				range.addEventListener("input",function() {
-					var v = parseInt(this.value);
-					var val = v * mult;
-					number.value = toFixed(val);
-					elem.dispatchEvent(new Event("change"));
-				});
-				number.addEventListener("change", function() {
-					var v = parseFloat(this.value);
-					var val = v / mult;
-					range.value = val;
-					elem.dispatchEvent(new Event("change"));
-				});				
-				env1.appendChild(range);
-				env2.appendChild(number);
-				elem.appendChild(env1);
-				elem.appendChild(env2);
-			}
-			range.value = val / mult;
-			number.value = toFixed(val);
-
-			
-		},
-		function(elem) {
-			var number = elem.querySelector("input[type=number]");
-			if (number) return parseFloat(number.valueAsNumber);
-			else return 0;
-			
-		},
-		function(elem,attrs) {
-			
-		}
-));
 
 App.prototype.init = function() {
 	this.strtable = document.getElementById("strtable").dataset;
---
+++ www/res/ui.js
@@ -0,0 +1,82 @@
+"use strict";
+
+TemplateJS.View.regCustomElement("X-SLIDER", new TemplateJS.CustomElement(
+		function(elem,val) {
+			var range = elem.querySelector("input[type=range]");
+			var number = elem.querySelector("input[type=number]");
+			var mult = parseFloat(elem.dataset.mult);
+			var fixed = parseInt(elem.dataset.fixed)
+			var toFixed = function(v) {
+				if (!isNaN(fixed)) return parseFloat(v).toFixed(fixed);
+				else return v;
+			}
+			if (!range) {
+				range = document.createElement("input");
+				range.setAttribute("type","range");
+				number = document.createElement("input");
+				number.setAttribute("type","number");
+				number.setAttribute("step",mult);
+				var env1 = document.createElement("div");
+				var env2 = document.createElement("div");
+				var min = parseFloat(elem.dataset.min);
+				var max = parseFloat(elem.dataset.max);
+				var rmin = Math.floor(min/mult);
+				var rmax = Math.floor(max/mult);
+				range.setAttribute("min",rmin);
+				range.setAttribute("max",rmax);
+				range.addEventListener("input",function() {
+					var v = parseInt(this.value);
+					var val = v * mult;
+					number.value = toFixed(val);
+					elem.dispatchEvent(new Event("change"));
+				});
+				number.addEventListener("change", function() {
+					var v = parseFloat(this.value);
+					var val = v / mult;
+					range.value = val;
+					elem.dispatchEvent(new Event("change"));
+				});				
+				env1.appendChild(range);
+				env2.appendChild(number);
+				elem.appendChild(env1);
+				elem.appendChild(env2);
+			}
+			range.value = val / mult;
+			number.value = toFixed(val);
+
+			
+		},
+		function(elem) {
+			var number = elem.querySelector("input[type=number]");
+			if (number) return parseFloat(number.valueAsNumber);
+			else return 0;
+			
+		},
+		function(elem,attrs) {
+			
+		}
+));
+
+
+TemplateJS.View.regCustomElement("X-CHECKBOX", new TemplateJS.CustomElement(
+		function(elem,val) {
+			var z = elem.querySelector("[data-name=inner");
+			if (z == null) {
+				z = document.createElement("span");
+				z.setAttribute("data-name","inner");				
+				elem.insertBefore(z, elem.firstChild);
+				var w = document.createElement("span");
+				z.appendChild(w);
+				
+				elem.addEventListener("click", function() {
+					elem.classList.toggle("checked");
+					elem.dispatchEvent(new Event("change"));
+				});
+			}
+			elem.classList.toggle("checked", val);
+		},
+		function(elem) {
+			return elem.classList.contains("checked");
+		}
+		
+));
